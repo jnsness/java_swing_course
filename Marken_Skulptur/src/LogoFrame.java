b5f2dcdaaef6af35e0bfd0880a1148e770a62844
Jnsness <jonas.stumm@googlemail.com>
2015-08-07 12:53:38 +0200

    added Layouts, Redo-Button and erased unneccesary Frame in LogoFrame

--- Marken_Skulptur/src/LogoFrame.java
+++ Marken_Skulptur/src/LogoFrame.java
@@ -1,31 +1,76 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
+import javax.swing.JButton;
+import javax.swing.BoxLayout;
 
 public class LogoFrame extends JFrame {
 
-	public static JFrame frame;
-	public static JPanel dragPanel;
+	private static JPanel dragPanel;
+	private JPanel buttonPanel;
+	private JButton btnRedo;
+	private JButton btnSave;
 
 	public LogoFrame() {
-		frame = new JFrame("Logo DragNDrop");
-		frame.setVisible(true);
-		frame.setSize(800, 600);
-		frame.setLayout(new BorderLayout());
-
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+		
+		getContentPane().setLayout(new BorderLayout());
+		
+		setVisible(true);
+		setSize(800, 600);
+		setResizable(false);
+		
+//		Set Location to Location of previous Window
+		setLocation(FormularWindow.frameLocation);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+// dragPanel Configuration
 		dragPanel = new JPanel();
 		dragPanel.setLayout(null);
-		frame.add(dragPanel, BorderLayout.CENTER);
+		dragPanel.setPreferredSize(new Dimension(600,0));
+		dragPanel.setBorder(BorderFactory.createEtchedBorder());
+		
+//		buttonPanel Configuration
+		
+		buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout());
+		buttonPanel.setPreferredSize(new Dimension(180,0));
+		buttonPanel.setBorder(BorderFactory.createEtchedBorder());
+		
+		btnRedo = new JButton();
+		btnRedo.setText("Redo");
+		buttonPanel.add(btnRedo);
+		
+		btnSave = new JButton();
+		btnSave.setText("Save");
+		buttonPanel.add(btnSave);
+		
+
+		
+//		add Panels to Frame
+		
+		getContentPane().add(buttonPanel, BorderLayout.EAST);
+		getContentPane().add(dragPanel, BorderLayout.WEST);
+		
+		btnRedo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				loadLogos();
+			}
+		});
 
 		/*
 		 * Is good create a Thread to manipulate Forms and Files. In this
@@ -49,14 +94,14 @@ public class LogoFrame extends JFrame {
 
 		// Array to give every image from folder
 
-		String imageUrl = "images/"+FormularWindow.collectValues();
+		String imageUrl = "images/" + FormularWindow.collectValues();
 		System.out.println(imageUrl);
 		File f = new File(imageUrl);
 		File[] fa = f.listFiles();
 
 		for (int i = 0; i <= 29; i++) {
 			String fileName = String.valueOf(fa[i]);
-			
+
 			addNewLogo(fileName, i);
 		}
 
